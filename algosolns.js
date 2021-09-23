f7f4cb57f4d527a67d98435cd00304efedddb59f
enaburekhan <ericenaburekhan2018@gmail.com>
2021-09-23 11:35:29 +0100

    Solved removing a value into a BST using recursion

--- algosolns.js
+++ algosolns.js
@@ -46,3 +46,47 @@ class BST {
                 return true
             }
       }
+
+      remove(value, parent = null) {
+        // Average case: O(logn) time | O(logn) space.
+        // Worst case: O(logn) time | O(logn) space.d.
+            if (value < this.value){
+                if (this.left !== null){
+                    this.left.remove(value, this)
+                }
+            }else if (value > this.value){
+                if (this.right !== null){
+                    this.right.remove(value, this)
+                }
+            }else {
+                if (this.left !== null && this.right !== null){
+                    this.value = this.right.getMinValue();
+                    this.right.remove(this.value, this)
+                }else if (parent === null){
+                    if (this.left !== null){
+                        this.value = this.left.value;
+                        this.right = this.left.right;
+                        this.left = this.left.left;
+                    }else if (this.right !== null){
+                        this.value = this.right.value;
+                        this.left = this.right.left;
+                        this.right = this.right.right;
+                    }
+                }else if (parent.left === this){
+                    parent.left = this.left !== null ? this.left : this.right;
+                }else if (parent.right === this){
+                    parent.right = this.left !== null ? this.left : this.right;
+                }
+            }
+        return this;
+      }
+            
+        getMinValue(){
+            if (this.left === null){
+                return this.value;
+            }else {
+                return this.left.getMinValue();
+            }
+        }	
+    }
+    
