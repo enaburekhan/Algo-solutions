8748fbfcf3be9521f04f7513e13bea15b80c2e55
enaburekhan <ericenaburekhan2018@gmail.com>
2021-10-16 13:19:50 +0100

    Solved Single cycle check

--- algosolns.js
+++ algosolns.js
@@ -714,3 +714,29 @@ function kadanesAlgorithm(array) {
 
 // Do not edit the line below.
 exports.kadanesAlgorithm = kadanesAlgorithm;
+
+
+
+
+/* Single Cycle Check */
+
+function hasSingleCycle(array) {
+  // O(n) time | O(1) where n is the length of the array.
+	let numElementsVisited = 0;
+	let currentIdx = 0;
+	while(numElementsVisited < array.length){
+		if(numElementsVisited > 0 && currentIdx === 0) return false;
+		numElementsVisited++;
+		currentIdx = getNextIdx(currentIdx, array)
+	}
+	 return currentIdx === 0
+}
+
+function getNextIdx(currentIdx, array){
+	const jump = array[currentIdx];
+	const nextIdx = (currentIdx + jump) % array.length;
+	return nextIdx >= 0 ? nextIdx : nextIdx + array.length;
+}
+
+// Do not edit the line below.
+exports.hasSingleCycle = hasSingleCycle;
