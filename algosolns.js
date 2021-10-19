5995e762e7ffb53f34defb0adb2328974855973f
enaburekhan <ericenaburekhan2018@gmail.com>
2021-10-19 10:30:19 +0100

    Solved twoNumberSum with O(nlogn) time and O(1) space

--- algosolns.js
+++ algosolns.js
@@ -780,6 +780,24 @@ exports.twoNumberSum = twoNumberSum;
 
 
 
+function twoNumberSum(array, targetSum) {
+  // O(nlogn) time | O(1) space.
+	array.sort((a, b) => a - b);
+	let left = 0;
+	let right = array.length - 1;
+	while(left < right){
+		const currentSum = array[left] + array[right];
+		if(currentSum === targetSum) return [array[left], array[right]];
+		else if(currentSum < targetSum) left++;
+	  else if(currentSum > targetSum) right--;
+	}
+	return [];
+}
+
+// Do not edit the line below.
+exports.twoNumberSum = twoNumberSum;
+
+
 
 // const getSorted = (arr) => {
 //   let i = 0;
