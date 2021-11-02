b9d2769871ed3553282d9104d303282d167eb3b5
enaburekhan <ericenaburekhan2018@gmail.com>
2021-11-02 10:12:47 +0100

    Solved closest value in Bst using O(logn) space

--- algosolns.js
+++ algosolns.js
@@ -1045,6 +1045,43 @@ function sortedSquaredArray(array) {
   
   // Do not edit the line below.
   exports.findClosestValueInBst = findClosestValueInBst;
+
+
+  function findClosestValueInBst(tree, target) {
+    // Avg case: O(logn) time | O(logn) space
+      // worst case: O(n) time | O(n) space
+      
+      return findClosestValueInBstHelper(tree, target, tree.value)
+  }
+  
+  function findClosestValueInBstHelper(tree, target, closest){
+      if(tree === null) return closest;
+      
+      if(Math.abs(target - closest) > Math.abs(target - tree.value)){
+          closest = tree.value
+      }
+      
+      if(target < tree.value){
+          return findClosestValueInBstHelper(tree.left, target, closest)
+      }else if(target > tree.value){
+          return findClosestValueInBstHelper(tree.right, target, closest)
+      }else{
+          return closest
+      }
+  }
+  
+  // This is the class of the input tree. Do not edit.
+  class BST {
+    constructor(value) {
+      this.value = value;
+      this.left = null;
+      this.right = null;
+    }
+  }
+  
+  // Do not edit the line below.
+  exports.findClosestValueInBst = findClosestValueInBst;
+  
   
   
   
