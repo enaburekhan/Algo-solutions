46dcdb13de85c27633b182e4225eaae8a61f4061
enaburekhan <ericenaburekhan2018@gmail.com>
2021-11-23 08:39:43 +0100

    Solve node depth using recursion

--- algosolns.js
+++ algosolns.js
@@ -1302,7 +1302,8 @@ exports.BinaryTree = BinaryTree;
 exports.branchSums = branchSums;
 
 
-/* Node depths */
+/* Node depths using iteration */
+
 function nodeDepths(root) {
   // O(n) time | O(d) space.
 	let sumOfDepths = 0;
@@ -1330,5 +1331,29 @@ class BinaryTree {
 
 // Do not edit the line below.
 exports.nodeDepths = nodeDepths;
+
+
+
+
+/* Solve nodeDepths using recursion */
+
+function nodeDepths(root, depth = 0) {
+  // O(n) time | O(d) space.
+	if(!root) return 0
+	return depth + nodeDepths(root.left, depth + 1) + nodeDepths(root.right, depth + 1)
+}
+
+// This is the class of the input binary tree.
+class BinaryTree {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+// Do not edit the line below.
+exports.nodeDepths = nodeDepths;
+
 
 
