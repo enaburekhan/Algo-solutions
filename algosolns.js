aebac6a83467c5d35b85b451c4cb1d8f332e027c
enaburekhan <ericenaburekhan2018@gmail.com>
2021-11-23 08:37:58 +0100

    Solve node depth using iteration

--- algosolns.js
+++ algosolns.js
@@ -1301,3 +1301,34 @@ function calculateBranchSums(node, runningSum, sums){
 exports.BinaryTree = BinaryTree;
 exports.branchSums = branchSums;
 
+
+/* Node depths */
+function nodeDepths(root) {
+  // O(n) time | O(d) space.
+	let sumOfDepths = 0;
+	let stack = [{node: root, depth: 0 }]
+	
+	while(stack.length > 0){
+		const {node, depth} = stack.pop();
+		if(!node) continue
+		sumOfDepths += depth;
+		
+		stack.push({ node: node.left, depth: depth + 1 })
+		stack.push({ node: node.right, depth: depth + 1 })
+	}
+	return sumOfDepths
+}
+
+// This is the class of the input binary tree.
+class BinaryTree {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+// Do not edit the line below.
+exports.nodeDepths = nodeDepths;
+
+
