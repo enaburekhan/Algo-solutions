d248bab648132d88ed3b0ce68b6a7608e4e4d62c
enaburekhan <ericenaburekhan2018@gmail.com>
2021-11-24 09:48:32 +0100

    Solve depthFirstSearch

--- algosolns.js
+++ algosolns.js
@@ -1357,3 +1357,36 @@ exports.nodeDepths = nodeDepths;
 
 
 
+
+// Solve depth depthFirstSearch
+
+// Do not edit the class below except
+// for the depthFirstSearch method.
+// Feel free to add new properties
+// and methods to the class.
+class Node {
+  constructor(name) {
+    this.name = name;
+    this.children = [];
+  }
+
+  addChild(name) {
+    this.children.push(new Node(name));
+    return this;
+  }
+
+  depthFirstSearch(array) {
+    // O(v + e) time | O(e).
+		array.push(this.name);
+		
+		for(const child of this.children){
+			child.depthFirstSearch(array)
+		}
+		return array;
+  }
+}
+
+// Do not edit the line below.
+exports.Node = Node;
+
+
