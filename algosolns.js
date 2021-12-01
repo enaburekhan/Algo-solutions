5db5b050d72f94cdea0106015569953ddb87c3ea
enaburekhan <ericenaburekhan2018@gmail.com>
2021-12-01 06:47:58 +0100

    Solve remove duplicates

--- algosolns.js
+++ algosolns.js
@@ -1508,3 +1508,31 @@ function reverseColorSpeeds(array){
 // Do not edit the line below.
 exports.tandemBicycle = tandemBicycle;
 
+
+/* Remove duplicates from LinkedList */
+// This is an input class. Do not edit.
+class LinkedList {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+function removeDuplicatesFromLinkedList(linkedList) {
+  // O(n) time | O(1) space.
+  let currentNode = linkedList;
+	while(currentNode !== null){
+		let nextDistinctNode = currentNode.next;
+		while(nextDistinctNode !== null && nextDistinctNode.value === currentNode.value){
+			nextDistinctNode = nextDistinctNode.next
+		}
+		currentNode.next = nextDistinctNode
+		currentNode = nextDistinctNode
+	}
+	return linkedList
+}
+
+// Do not edit the lines below.
+exports.LinkedList = LinkedList;
+exports.removeDuplicatesFromLinkedList = removeDuplicatesFromLinkedList;
+
