2d442874f2fc03669e772c5ddd86a37dc48a6e6b
enaburekhan <ericenaburekhan2018@gmail.com>
2021-12-06 09:26:35 +0100

    Solve fibonacci sequence using O(n) time and O(1) space

--- algosolns.js
+++ algosolns.js
@@ -1561,3 +1561,21 @@ function getNthFib(n, memoize={1:0, 2:1}) {
 // Do not edit the line below.
 exports.getNthFib = getNthFib;
 
+
+function getNthFib(n) {
+  // O(n) time | O(1) space.
+	let lastTwo = [0, 1];
+	let counter = 3
+	while(counter <= n){
+		const nextFib = lastTwo[0] + lastTwo[1];
+		lastTwo[0] = lastTwo[1];
+		lastTwo[1] = nextFib;
+		counter += 1;
+	}
+	return n > 1 ? lastTwo[1] : lastTwo[0];
+}
+
+// Do not edit the line below.
+exports.getNthFib = getNthFib;
+
+
