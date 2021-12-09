b881a1a976db3c5244cecf22044335ec02cb6856
enaburekhan <ericenaburekhan2018@gmail.com>
2021-12-09 11:16:18 +0100

    Solve find three largest numbers

--- algosolns.js
+++ algosolns.js
@@ -1650,3 +1650,37 @@ function binarySearchHelper(array, target, left, right){
 exports.binarySearch = binarySearch;
 
 
+/* Solve find three largest numbers */
+
+function findThreeLargestNumbers(array) {
+  // O(n) time | O(1) space.
+	const threeLargest = [null, null, null];
+	for(const num of array){
+		updateLargest(threeLargest, num)
+	}
+	return threeLargest
+}
+
+function updateLargest(threeLargest, num){
+	if(threeLargest[2] === null || num > threeLargest[2]){
+		updateAndShift(threeLargest, num, 2)
+	}else if(threeLargest[1] === null || num > threeLargest[1]){
+		updateAndShift(threeLargest, num, 1)
+	}else if(threeLargest[0] === null || num > threeLargest[0]){
+		updateAndShift(threeLargest, num, 0)
+	}
+}
+
+function updateAndShift(array, num, idx){
+	for(let i=0; i <= idx; i++){
+		if(i === idx){
+			array[i] = num
+		}else{
+			array[i] = array[i + 1]
+		}
+	}
+}
+
+// Do not edit the line below.
+exports.findThreeLargestNumbers = findThreeLargestNumbers;
+
