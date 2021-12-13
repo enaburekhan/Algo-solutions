648a241351e97c037d3cfd3f3ddcc0e59f8fee27
enaburekhan <ericenaburekhan2018@gmail.com>
2021-12-13 09:00:24 +0100

    Solve selection sort

--- algosolns.js
+++ algosolns.js
@@ -1768,4 +1768,27 @@ function swap(i, j, array){
 exports.insertionSort = insertionSort;
 
 
+/* Solve selection sort */
+
+function selectionSort(array) {
+  // O(n^2) time | O(1) space.	
+	let startIdx = 0
+	while(startIdx < array.length - 1){
+		let smallestIdx = startIdx
+		for(let i = startIdx + 1; i < array.length; i++){
+			if(array[smallestIdx] > array[i]) smallestIdx = i
+		}
+		swap(startIdx, smallestIdx, array);
+		startIdx++
+	}
+	return array
+}
+
+function swap(i, j, array){
+	[array[i], array[j]] = [array[j], array[i]]
+}
+
+// Do not edit the line below.
+exports.selectionSort = selectionSort;
+
 
