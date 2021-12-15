e8f999630dd1bdccf154bd1c52dbd52e6788a569
enaburekhan <ericenaburekhan2018@gmail.com>
2021-12-15 10:10:44 +0100

    Solve caesar cipher encryption 1

--- algosolns.js
+++ algosolns.js
@@ -1809,4 +1809,28 @@ function isPalindrome(string) {
 exports.isPalindrome = isPalindrome;
 
 
+/* Solve Caesar Cipher Encryptor */
+
+function caesarCipherEncryptor(string, key) {
+  // O(n) time | O(n) space
+	const newLetters = [];
+	const newKey = key % 26;
+	for(const letter of string){
+		newLetters.push(getLetters(letter, newKey))
+	}
+	return newLetters.join("");
+}
+
+function getLetters(letter, key){
+	const newLetterCode = letter.charCodeAt() + key;
+	
+	return newLetterCode <= 122 ? String.fromCharCode(newLetterCode) :
+	  String.fromCharCode(96 + (newLetterCode % 122));
+}
+
+// Do not edit the line below.
+exports.caesarCipherEncryptor = caesarCipherEncryptor;
+
+
+
 
