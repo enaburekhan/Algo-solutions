5fe8faf3ca0e067ae36a212e67350440496cdad2
enaburekhan <ericenaburekhan2018@gmail.com>
2021-12-21 10:19:57 +0100

    Solve generate document with O(c *(m+n)) time

--- algosolns.js
+++ algosolns.js
@@ -2121,5 +2121,33 @@
   
   // Do not edit the line below.
   exports.generateDocument = generateDocument;
+
+
+  function generateDocument(characters, document) {
+    // O(c * (m + n)) time | O(c) space.
+    const alreadyCounted = new Set();
+    for(const character of document){
+      if(character in alreadyCounted) continue
+      
+      const documentFrequency = countCharacterFrequency(character, document);
+      const characterFrequency = countCharacterFrequency(character, characters);
+      if(documentFrequency > characterFrequency) return false
+      
+      alreadyCounted.add(character);
+    }
+     return true;
+  }
+  
+  function countCharacterFrequency(character, target){
+    let frequency = 0;
+    for(const char of target){
+      if(char === character) frequency++
+    }
+     return frequency
+  }
+  
+  // Do not edit the line below.
+  exports.generateDocument = generateDocument;
+  
   
 
