7f9e19d88159f4730e89eeb2c1a52c7d730521d8
enaburekhan <ericenaburekhan2018@gmail.com>
2021-12-23 09:26:21 +0100

    Solve three number sum

--- algosolns.js
+++ algosolns.js
@@ -2205,7 +2205,37 @@
   exports.firstNonRepeatingCharacter = firstNonRepeatingCharacter;
   
   
+
+
+  /* Solve three number sum */
   
+  function threeNumberSum(array, targetSum) {
+    // O(n^2) time | O(n) space
+    const triplets = []
+    array.sort((a,b) => a - b);
+    for(let i=0; i < array.length - 2; i++){
+      let left = i + 1;
+      let right = array.length - 1;
+      
+      while(left < right){
+        const currentSum = array[i] + array[left] + array[right];
+        if(currentSum === targetSum){
+          triplets.push([array[i], array[left], array[right]]);
+          left++;
+          right--
+        }else if(currentSum < targetSum){
+          left++
+        }else if(currentSum > targetSum){
+          right--
+        }
+      }
+    }
+     return triplets;
+  }
+  
+  // Do not edit the line below.
+  exports.threeNumberSum = threeNumberSum;
+    
   
   
 
