dbbc593db677ca4c11c41c5e35bfa36ca2b831b4
enaburekhan <ericenaburekhan2018@gmail.com>
2021-12-29 10:30:18 +0100

    Solve monotonic arrays using breaksDirection helper function

--- algosolns.js
+++ algosolns.js
@@ -2358,6 +2358,36 @@ function swap(i, j, array){
 exports.moveElementToEnd = moveElementToEnd;
 
 
+/* Solved Monotonic arrays */
+
+function isMonotonic(array) {
+  // O(n) time | O(1) space.
+	if(array.length <= 2) return true
+	let direction = array[1] - array[0];
+	
+	for(let i=1; i < array.length; i++){
+		if(direction === 0){
+			direction = array[i] - array[i-1]
+			continue;
+		}
+		if(breaksDirection(direction, array[i-1], array[i])){
+			return false
+		}
+	}
+	 return true;
+}
+
+function breaksDirection(direction, previousInt, currentInt){
+	const difference = currentInt - previousInt
+	if(direction > 0) return difference < 0;
+	return difference > 0
+}
+
+// Do not edit the line below.
+exports.isMonotonic = isMonotonic;
+
+
+
 
     
   
