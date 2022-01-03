7c4fde181cb0279586afcfd97eb7709cf1e49745
enaburekhan <ericenaburekhan2018@gmail.com>
2022-01-03 10:28:55 +0100

    Solve array of products using O(n) time

--- algosolns.js
+++ algosolns.js
@@ -2527,6 +2527,34 @@ function arrayOfProducts(array) {
 exports.arrayOfProducts = arrayOfProducts;
 
 
+function arrayOfProducts(array) {
+  // O(n) time | O(n) space.
+	const products = new Array(array.length).fill(1);
+	const leftProducts = new Array(array.length).fill(1);
+	const rightProducts = new Array(array.length).fill(1);
+	
+	let leftRunningProduct = 1;
+	for(let i = 0; i < array.length; i++){
+		leftProducts[i] = leftRunningProduct
+		leftRunningProduct *= array[i]
+	}
+	
+	let rightRunningProduct = 1;
+	for(let i = array.length - 1; i > -1; i-- ){
+		rightProducts[i] = rightRunningProduct
+		rightRunningProduct *= array[i]
+	}
+	
+	for(let i = 0; i < array.length; i++){
+		products[i] = leftProducts[i] * rightProducts[i]
+	}
+	return products
+}
+
+// Do not edit the line below.
+exports.arrayOfProducts = arrayOfProducts;
+
+
 
 
 
