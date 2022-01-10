71cc934cce09a26c7175be0a9c6a4adcf64a1ac3
enaburekhan <ericenaburekhan2018@gmail.com>
2022-01-10 11:17:20 +0100

    Solve min height Bst using O(nlogn) time

--- algosolns.js
+++ algosolns.js
@@ -2718,41 +2718,91 @@
 /* Solve BST traversal */
 
 // O(n) time | O(n) space
-function inOrderTraverse(tree, array) {
+// function inOrderTraverse(tree, array) {
   // Write your code here.
-	if(tree !== null){
-		inOrderTraverse(tree.left, array);
-		array.push(tree.value);
-		inOrderTraverse(tree.right, array)
-	}
-	return array
-}
+// 	if(tree !== null){
+// 		inOrderTraverse(tree.left, array);
+// 		array.push(tree.value);
+// 		inOrderTraverse(tree.right, array)
+// 	}
+// 	return array
+// }
 
 // O(n) time | O(n) space
-function preOrderTraverse(tree, array) {
+// function preOrderTraverse(tree, array) {
   // Write your code here.
-	if(tree !== null){
-		array.push(tree.value);
-	preOrderTraverse(tree.left, array);
-	preOrderTraverse(tree.right, array);
-}
-	return array
-}
+// 	if(tree !== null){
+// 		array.push(tree.value);
+// 	preOrderTraverse(tree.left, array);
+// 	preOrderTraverse(tree.right, array);
+// }
+// 	return array
+// }
 
 // O(n) time | O(n) space
-function postOrderTraverse(tree, array) {
+// function postOrderTraverse(tree, array) {
   // Write your code here.
-	if(tree !== null){
-		postOrderTraverse(tree.left, array);
-		postOrderTraverse(tree.right, array);
-		array.push(tree.value)
-	}
-	 return array
-}
+// 	if(tree !== null){
+// 		postOrderTraverse(tree.left, array);
+// 		postOrderTraverse(tree.right, array);
+// 		array.push(tree.value)
+// 	}
+// 	 return array
+// }
 
 // Do not edit the lines below.
-exports.inOrderTraverse = inOrderTraverse;
-exports.preOrderTraverse = preOrderTraverse;
-exports.postOrderTraverse = postOrderTraverse;
+// exports.inOrderTraverse = inOrderTraverse;
+// exports.preOrderTraverse = preOrderTraverse;
+// exports.postOrderTraverse = postOrderTraverse;
 
 
+/* Solve min Height BST */
+
+function minHeightBst(array) {
+  // O(nlogn) time | O(n) space.
+	return constructMinHeightBst(array, null, 0, array.length - 1)
+}
+
+function constructMinHeightBst(array, bst, startIdx, endIdx){
+	if(endIdx < startIdx) return
+	
+	const midIdx = Math.floor((startIdx + endIdx) / 2);
+	const valueToAdd = array[midIdx];
+	
+	if(bst === null){
+		bst = new BST(valueToAdd)
+	}else{
+		bst.insert(valueToAdd)
+	}
+	
+	constructMinHeightBst(array, bst, startIdx, midIdx - 1);
+	constructMinHeightBst(array, bst, midIdx + 1, endIdx);
+	return bst
+}
+
+class BST {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(value) {
+    if (value < this.value) {
+      if (this.left === null) {
+        this.left = new BST(value);
+      } else {
+        this.left.insert(value);
+      }
+    } else {
+      if (this.right === null) {
+        this.right = new BST(value);
+      } else {
+        this.right.insert(value);
+      }
+    }
+  }
+}
+
+// Do not edit the line below.
+exports.minHeightBst = minHeightBst;
