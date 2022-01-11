1f3d370f1fc3c491a29508391233f501652508c8
enaburekhan <ericenaburekhan2018@gmail.com>
2022-01-11 11:25:32 +0100

    Solve find Kth value using brute force approach

--- algosolns.js
+++ algosolns.js
@@ -2758,95 +2758,125 @@
 
 /* Solve min Height BST */
 
-function minHeightBst(array) {
-  // O(nlogn) time | O(n) space.
-	return constructMinHeightBst(array, null, 0, array.length - 1)
-}
+// function minHeightBst(array) {
+//   // O(nlogn) time | O(n) space.
+// 	return constructMinHeightBst(array, null, 0, array.length - 1)
+// }
 
-function constructMinHeightBst(array, bst, startIdx, endIdx){
-	if(endIdx < startIdx) return
+// function constructMinHeightBst(array, bst, startIdx, endIdx){
+// 	if(endIdx < startIdx) return
 	
-	const midIdx = Math.floor((startIdx + endIdx) / 2);
-	const valueToAdd = array[midIdx];
+// 	const midIdx = Math.floor((startIdx + endIdx) / 2);
+// 	const valueToAdd = array[midIdx];
 	
-	if(bst === null){
-		bst = new BST(valueToAdd)
-	}else{
-		bst.insert(valueToAdd)
-	}
+// 	if(bst === null){
+// 		bst = new BST(valueToAdd)
+// 	}else{
+// 		bst.insert(valueToAdd)
+// 	}
 	
-	constructMinHeightBst(array, bst, startIdx, midIdx - 1);
-	constructMinHeightBst(array, bst, midIdx + 1, endIdx);
-	return bst
-}
+// 	constructMinHeightBst(array, bst, startIdx, midIdx - 1);
+// 	constructMinHeightBst(array, bst, midIdx + 1, endIdx);
+// 	return bst
+// }
 
+// class BST {
+//   constructor(value) {
+//     this.value = value;
+//     this.left = null;
+//     this.right = null;
+//   }
+
+//   insert(value) {
+//     if (value < this.value) {
+//       if (this.left === null) {
+//         this.left = new BST(value);
+//       } else {
+//         this.left.insert(value);
+//       }
+//     } else {
+//       if (this.right === null) {
+//         this.right = new BST(value);
+//       } else {
+//         this.right.insert(value);
+//       }
+//     }
+//   }
+// }
+
+// // Do not edit the line below.
+// exports.minHeightBst = minHeightBst;
+
+// function minHeightBst(array) {
+//   // Write your code here.
+// 	return constructMinHeightBst(array, null, 0, array.length - 1)
+// }
+
+// function constructMinHeightBst(array, bst, startIdx, endIdx){
+// 	if(endIdx < startIdx) return
+// 	const midIdx = Math.floor((startIdx + endIdx) / 2);
+// 	const newBstNode = new BST(array[midIdx]);
+	
+// 	if(bst === null){
+// 		bst = newBstNode
+// 	}else{
+// 		if(array[midIdx] < bst.value){
+// 			bst.left = newBstNode;
+// 			bst = bst.left;
+// 		}else{
+// 			bst.right = newBstNode;
+// 			bst = bst.right;
+// 		}
+// 	}
+	
+// 	constructMinHeightBst(array, bst, startIdx, midIdx - 1);
+// 	constructMinHeightBst(array, bst, midIdx + 1, endIdx);
+// 	return bst;
+// }
+
+// function minHeightBst(array) {
+//   // O(n) time | O(n) space.
+// 	return constructMinHeightBst(array, 0, array.length - 1);
+// }
+
+// function constructMinHeightBst(array, startIdx, endIdx){
+// 	if(endIdx < startIdx) return null;
+	
+// 	const midIdx = Math.floor((startIdx + endIdx) / 2);
+// 	const bst = new BST(array[midIdx]);
+	
+// 	bst.left = constructMinHeightBst(array, startIdx, midIdx - 1);
+// 	bst.right = constructMinHeightBst(array, midIdx + 1, endIdx);
+// 	return bst;
+// }
+
+
+
+/* Solve find kth value */
+// This is an input class. Do not edit.
 class BST {
   constructor(value) {
     this.value = value;
     this.left = null;
     this.right = null;
   }
-
-  insert(value) {
-    if (value < this.value) {
-      if (this.left === null) {
-        this.left = new BST(value);
-      } else {
-        this.left.insert(value);
-      }
-    } else {
-      if (this.right === null) {
-        this.right = new BST(value);
-      } else {
-        this.right.insert(value);
-      }
-    }
-  }
 }
 
-// Do not edit the line below.
-exports.minHeightBst = minHeightBst;
-
-function minHeightBst(array) {
-  // Write your code here.
-	return constructMinHeightBst(array, null, 0, array.length - 1)
-}
-
-function constructMinHeightBst(array, bst, startIdx, endIdx){
-	if(endIdx < startIdx) return
-	const midIdx = Math.floor((startIdx + endIdx) / 2);
-	const newBstNode = new BST(array[midIdx]);
-	
-	if(bst === null){
-		bst = newBstNode
-	}else{
-		if(array[midIdx] < bst.value){
-			bst.left = newBstNode;
-			bst = bst.left;
-		}else{
-			bst.right = newBstNode;
-			bst = bst.right;
-		}
-	}
-	
-	constructMinHeightBst(array, bst, startIdx, midIdx - 1);
-	constructMinHeightBst(array, bst, midIdx + 1, endIdx);
-	return bst;
-}
-
-function minHeightBst(array) {
+function findKthLargestValueInBst(tree, k) {
   // O(n) time | O(n) space.
-	return constructMinHeightBst(array, 0, array.length - 1);
+  const sortedNodeValues = [];
+	inOrderTraverse(tree, sortedNodeValues)
+	return sortedNodeValues[sortedNodeValues.length - k]
 }
 
-function constructMinHeightBst(array, startIdx, endIdx){
-	if(endIdx < startIdx) return null;
+function inOrderTraverse(node, sortedNodeValues){
+	if(node === null) return;
 	
-	const midIdx = Math.floor((startIdx + endIdx) / 2);
-	const bst = new BST(array[midIdx]);
-	
-	bst.left = constructMinHeightBst(array, startIdx, midIdx - 1);
-	bst.right = constructMinHeightBst(array, midIdx + 1, endIdx);
-	return bst;
+	inOrderTraverse(node.left, sortedNodeValues);
+	sortedNodeValues.push(node.value);
+	inOrderTraverse(node.right, sortedNodeValues)
 }
 
+// Do not edit the lines below.
+exports.BST = BST;
+exports.findKthLargestValueInBst = findKthLargestValueInBst;
