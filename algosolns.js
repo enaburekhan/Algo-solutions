cb1a6c705de9de98272336d10868ee19e0ebc278
enaburekhan <ericenaburekhan2018@gmail.com>
2022-02-10 11:55:29 +0100

    Solve tournamentWinner

--- algosolns.js
+++ algosolns.js
@@ -3390,3 +3390,32 @@ function sortedSquaredArray(array) {
 
 // Do not edit the line below.
 exports.sortedSquaredArray = sortedSquaredArray;
+
+
+function tournamentWinner(competitions, results) {
+  // O(n) time | O(k) space.
+  const Home_Team_Won = 1;
+	let currentBestTeam = "";
+	const scores = {[currentBestTeam]: 0 }
+	
+	for(let idx = 0; idx < competitions.length; idx++){
+		const result = results[idx];
+		const [homeTeam, awayTeam] = competitions[idx];
+		const winningTeam = result === Home_Team_Won ? homeTeam : awayTeam
+		
+		updateScores(winningTeam, 3, scores)
+		if(scores[winningTeam] > scores[currentBestTeam]){
+			currentBestTeam = winningTeam;
+		}
+	}
+	 return currentBestTeam;
+}
+
+function updateScores(team, points, scores){
+	if(!(team in scores)) scores[team] = 0;
+	scores[team] += points
+}
+
+// Do not edit the line below.
+exports.tournamentWinner = tournamentWinner;
+
