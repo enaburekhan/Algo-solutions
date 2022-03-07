bfb6ffd428b0e8487d649fe10e554e81f409ba9e
enaburekhan <ericenaburekhan2018@gmail.com>
2022-03-07 10:27:24 +0100

    Solve first duplicate value using O(n) time and O(1) space

--- algosolns.js
+++ algosolns.js
@@ -3747,4 +3747,13 @@ function firstDuplicateValue(array) {
 	 return -1
 }
 
+function firstDuplicateValue(array) {
+  // O(n) time | O(1) space - where n is the length of the input array.
+  for(const value of array){
+		const absValue = Math.abs(value)
+		if(array[absValue - 1] < 0) return absValue
+		array[absValue - 1] *= -1
+	}
+	 return -1
+}
 
