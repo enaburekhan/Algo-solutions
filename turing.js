8e81f0420126a4cb5630ec344d5e8bb469715e77
enaburekhan <ericenaburekhan2018@gmail.com>
2022-06-28 01:18:32 +0100

    Solve isToeplitz

--- turing.js
+++ turing.js
@@ -139,54 +139,77 @@
 
 // console.log(operator.id);
 
-function zigzagTraverse(array) {
-  const height = array.length - 1;
-  const width = array[0].length - 1;
-  let row = 0;
-  let col = 0;
-  const result = [];
-  let goingDown = true;
+// function zigzagTraverse(array) {
+//   const height = array.length - 1;
+//   const width = array[0].length - 1;
+//   let row = 0;
+//   let col = 0;
+//   const result = [];
+//   let goingDown = true;
 
-  while(!isOutOfBounds(row, col, height, width)){
-    result.push(array[row][col])
-    if(goingDown){
-      if(col === 0 || row === height){
-        goingDown = false;
-        if(row === height){
-           col += 1;
-        }else {
-          row += 1
-        }
-      }else{
-        row += 1;
-        col -= 1;
-      }
-    }else{
-      if(row === 0 || col === width){
-        goingDown = true;
-        if(col === width){
-          row += 1;
-        }else {
-          col += 1
-        }
-      }else{
-        row -= 1;
-        col += 1;
+//   while(!isOutOfBounds(row, col, height, width)){
+//     result.push(array[row][col])
+//     if(goingDown){
+//       if(col === 0 || row === height){
+//         goingDown = false;
+//         if(row === height){
+//            col += 1;
+//         }else {
+//           row += 1
+//         }
+//       }else{
+//         row += 1;
+//         col -= 1;
+//       }
+//     }else{
+//       if(row === 0 || col === width){
+//         goingDown = true;
+//         if(col === width){
+//           row += 1;
+//         }else {
+//           col += 1
+//         }
+//       }else{
+//         row -= 1;
+//         col += 1;
+//       }
+//     }
+//   }
+//    return result
+// }
+
+// function isOutOfBounds(row, col, height, width){
+//   return row < 0 || row > height || col < 0 || col > width
+// }
+
+// const array = [
+//   [1, 3, 4, 10],
+//   [2, 5, 9, 11],
+//   [6, 8, 12, 15],
+//   [7, 13, 14, 16],
+// ]
+
+// console.log(zigzagTraverse(array)); // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+
+function isToeplitz(arr) {
+  for(let row=0; row < arr.length; row++){
+    for(let col=0; col < arr[0].length; col++){
+      if(arr[row+1] !== undefined  // has next row
+         && arr[row + 1][col + 1] !== undefined   // has next column
+         && arr[row + 1][col + 1] !== arr[row][col]  // 
+        ){
+        return false
       }
     }
   }
-   return result
+   return true
 }
+const arr1 = [[1,2,3,4],
+              [5,1,2,3],
+              [6,5,1,2]]
 
-function isOutOfBounds(row, col, height, width){
-  return row < 0 || row > height || col < 0 || col > width
-}
+const arr2 = [[1,2,3,4],
+              [5,1,9,3],
+              [6,5,1,2]]              
 
-const array = [
-  [1, 3, 4, 10],
-  [2, 5, 9, 11],
-  [6, 8, 12, 15],
-  [7, 13, 14, 16],
-]
-
-console.log(zigzagTraverse(array)); // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+console.log(isToeplitz(arr2));
