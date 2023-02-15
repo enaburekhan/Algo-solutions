796fa5d0710f91ba1153c720aeb4709437723eee
enaburekhan <ericenaburekhan2018@gmail.com>
2023-02-15 23:54:00 +0100

    Solve permutations

--- algosolns.js
+++ algosolns.js
@@ -3971,19 +3971,44 @@
 
 // console.log(findKthLargest(nums, k)); // 5
 
-var generateParenthesis = function (n) {
-  const res = [];
-  let iterate = (str, open, close) => {
-    if (open > n || close > n || close > open) return;
-    if (str.length === n * 2 && open === close) {
-      res.push(str);
-      return;
+// var generateParenthesis = function (n) {
+//   const res = [];
+//   let iterate = (str, open, close) => {
+//     if (open > n || close > n || close > open) return;
+//     if (str.length === n * 2 && open === close) {
+//       res.push(str);
+//       return;
+//     }
+//     iterate(str + '(', open + 1, close);
+//     iterate(str + ')', open, close + 1);
+//   };
+//   iterate('', 0, 0);
+//   return res;
+// };
+// n = 3;
+// console.log(generateParenthesis(n)); //["((()))","(()())","(())()","()(())","()()()"]
+
+function getPermutations(array) {
+  const permutations = [];
+  permutationsHelper(0, array, permutations);
+  return permutations;
+}
+
+function permutationsHelper(i, array, permutations) {
+  if (i === array.length - 1) {
+    permutations.push(array.slice());
+  } else {
+    for (let j = i; j < array.length; j++) {
+      swap(i, j, array);
+      permutationsHelper(i + 1, array, permutations);
+      swap(i, j, array);
     }
-    iterate(str + '(', open + 1, close);
-    iterate(str + ')', open, close + 1);
-  };
-  iterate('', 0, 0);
-  return res;
-};
-n = 3;
-console.log(generateParenthesis(n)); //["((()))","(()())","(())()","()(())","()()()"]
+  }
+}
+
+function swap(i, j, array) {
+  [array[i], array[j]] = [array[j], array[i]];
+}
+
+const array = [1, 2, 3];
+console.log(getPermutations(array));
